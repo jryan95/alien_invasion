8809d2aef3d1f89044dff8eeca6507f10dc6c772
Jon <jcryan00@gmail.com>
2020-07-16 10:46:53 -0500

    added movement functionality to our ship custom fullscreen toggle created

--- settings.py
+++ settings.py
@@ -7,3 +7,9 @@ class Settings:
         self.screen_width = 1080
         self.screen_height = 720
         self.bg_color = (230, 230, 230)
+
+        # Ship settings
+        self.ship_speed = 1.5
+
+        # Fullscreen flag
+        self.toggle_fullscreen = False
