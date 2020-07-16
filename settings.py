c7e6fc2d2732447366a2a9c1cdfa79008cb7e506
Jon <jcryan00@gmail.com>
2020-07-15 22:12:56 -0500

    alien invasion update

---
+++ settings.py
@@ -0,0 +1,9 @@
+class Settings:
+    """A class to store all the settings for Alien Invasion."""
+
+    def __init__(self):
+        """Initialize the game's settings."""
+        # Screen settings
+        self.screen_width = 1080
+        self.screen_height = 720
+        self.bg_color = (230, 230, 230)
