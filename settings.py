7ac09469b8fd1c030da186aa3c653ba5da17a990
Jon <jcryan00@gmail.com>
2020-07-17 19:23:16 -0500

    update

--- settings.py
+++ settings.py
@@ -13,3 +13,10 @@ class Settings:
 
         # Fullscreen flag
         self.toggle_fullscreen = False
+
+        # Bullet settings
+        self.bullet_speed = 1.0
+        self.bullet_width = 3
+        self.bullet_height = 15
+        self.bullet_color = (60, 60, 60)
+        self.bullets_allowed = 3
