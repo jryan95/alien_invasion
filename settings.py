5d6ae397867eb77ac71bb366d2667ac7265ba4a5
jryan95 <64394878+jryan95@users.noreply.github.com>
2020-07-18 23:25:55 -0500

    added fleet movement

--- settings.py
+++ settings.py
@@ -9,14 +9,20 @@ class Settings:
         self.bg_color = (230, 230, 230)
 
         # Ship settings
-        self.ship_speed = 1.5
+        self.ship_speed = 0.7
 
         # Fullscreen flag
         self.toggle_fullscreen = False
 
         # Bullet settings
-        self.bullet_speed = 1.0
+        self.bullet_speed = 0.3
         self.bullet_width = 3
         self.bullet_height = 15
         self.bullet_color = (60, 60, 60)
         self.bullets_allowed = 3
+
+        # Alien settings
+        self.alien_speed = 0.3
+        self.fleet_drop_speed = 1
+        # fleet_direction of 1 represents right; -1 represents left
+        self.fleet_direction = 1
